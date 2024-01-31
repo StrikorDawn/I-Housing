92c7c1f6f771ca60a4660fa58eb56d6b55260efc
Drew Howells <drewjhowells@gmail.com>
2024-01-30 17:23:50 -0700

    Revert "Idk"
    This reverts commit b4bf15f02a52900dd7d05d2ed62c0593daddac06.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This app is designed to provide an easily understood visual map of housing options in Rexburg Idaho for BYU-Idaho students
 
 # Development Environment
-- IntelliJ IDEA
+- Android Studio Hedgehog 2023.1.1
 - Github
 - Kotlin 1.9.22
 
--- app/src/main/java/com/example/i_housing/MainActivity.kt
+++ app/src/main/java/com/example/i_housing/MainActivity.kt
@@ -18,7 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             IHousingTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
