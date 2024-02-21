dfc145d715cbb166e1fbdb1b1a92aee00c531775
Drew Howells <drewjhowells@gmail.com>
2024-02-21 08:58:11 -0700

    Added compose files and integrated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+		vectorDrawables {
+			useSupportLibrary = true
+		}
+	}
 
     buildTypes {
         release {
@@ -52,7 +55,16 @@ android {
     }
     buildFeatures {
         viewBinding = true
-    }
+		compose = true
+	}
+	composeOptions {
+		kotlinCompilerExtensionVersion = "1.5.1"
+	}
+	packaging {
+		resources {
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+		}
+	}
 }
 
 dependencies {
@@ -62,7 +74,18 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
-    testImplementation("junit:junit:4.13.2")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+	implementation("androidx.activity:activity-compose:1.8.2")
+	implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+	implementation("androidx.compose.ui:ui")
+	implementation("androidx.compose.ui:ui-graphics")
+	implementation("androidx.compose.ui:ui-tooling-preview")
+	implementation("androidx.compose.material3:material3")
+	testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+	androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+	debugImplementation("androidx.compose.ui:ui-tooling")
+	debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "I-Housing"
 include(":app")
+include(":app")
  
