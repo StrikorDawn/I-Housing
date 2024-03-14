cf8b682b28e7839881895003f30f6c2d1dfed428
jasonrao <jasonrao@byui.edu>
2024-03-14 09:13:16 -0600

    database changes

--- app/src/main/java/com/example/i_housing/Apartment.kt
+++ app/src/main/java/com/example/i_housing/Apartment.kt
@@ -5,38 +5,38 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
-//@Entity(tableName = "Apartment")
-//data class Apartment(
-//	@PrimaryKey(autoGenerate = true)
-//	val id: Long = 0,
-//	val name: String,
-//	val price: Int,
-//	@ColumnInfo(name = "pay_schedule")
-//	val paySchedule: String,
-//	val website: String,
-//	@ColumnInfo(name = "phone_number")
-//	val phoneNumber: String,
-//	@ColumnInfo(name = "distance_to_campus")
-//	val distanceToCampus: String,
-//	val gym: Boolean,
-//	@ColumnInfo(name = "hot_tub")
-//	val hotTub: Boolean,
-//	@ColumnInfo(name = "club_house")
-//	val clubHouse: Boolean,
-//	@ColumnInfo(name = "washer_dryer")
-//	val washerDryer: Boolean,
-//	val fridge: String,
-//	val bathroom: String
-//)
-
+@Entity(tableName = "Apartment")
 data class Apartment(
-	val name : String,
-	val single : Boolean,
-	val price : Float,
-	val semester: Boolean,
-	val distanceFromCampus: Float,
-	val amenities : List<String>,
-	val link : String,
-	val phoneNumber : String
+	@PrimaryKey(autoGenerate = true)
+	val id: Long = 0,
+	val name: String,
+	val price: Int,
+	@ColumnInfo(name = "pay_schedule")
+	val paySchedule: String,
+	val website: String,
+	@ColumnInfo(name = "phone_number")
+	val phoneNumber: String,
+	@ColumnInfo(name = "distance_to_campus")
+	val distanceToCampus: String,
+	val gym: Boolean,
+	@ColumnInfo(name = "hot_tub")
+	val hotTub: Boolean,
+	@ColumnInfo(name = "club_house")
+	val clubHouse: Boolean,
+	@ColumnInfo(name = "washer_dryer")
+	val washerDryer: Boolean,
+	val fridge: String,
+	val bathroom: String
 )
+
+//data class Apartment(
+//	val name : String,
+//	val single : Boolean,
+//	val price : Float,
+//	val semester: Boolean,
+//	val distanceFromCampus: Float,
+//	val amenities : List<String>,
+//	val link : String,
+//	val phoneNumber : String
+//)
 
