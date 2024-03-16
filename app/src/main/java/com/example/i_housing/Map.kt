e3d2b917b1c349fb5d3886dac64e7050133a4eaa
Jandrus000 <jcandrus02@gmail.com>
2024-03-16 15:43:19 -0600

    Map composable implementation added to build gradle

--- app/src/main/java/com/example/i_housing/Map.kt
+++ app/src/main/java/com/example/i_housing/Map.kt
@@ -1,12 +1,26 @@
 package com.example.i_housing
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 @Composable
 fun MapApartments(navController: NavController) {
 	Text(text = "Map Goes Here", textAlign = TextAlign.Center)
 
 }
+}
+
+
