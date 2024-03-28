080f159ca94ef3170165282b96993efee52bd800
JeanKonan <kon22005@byui.edu>
2024-03-28 01:11:31 -0600

    jk--

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.material3:material3")
 	implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("com.google.maps.android:maps-compose: <4.3.3>")
+    implementation("com.google.maps.android:maps-compose:4.3.3")
     testImplementation("junit:junit:4.13.2")
     implementation("mysql:mysql-connector-java:8.0.23")
     testImplementation("junit:junit:4.13.2")
